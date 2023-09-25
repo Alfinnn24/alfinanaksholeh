326915cbd493b113df016400de98c5817bda93f9
Alfinnn24 <144185072+Alfinnn24@users.noreply.github.com>
2023-09-25 21:43:51 +0700

    Percobaan Pemilihan3

--- PemilihanPercobaan317.java
+++ PemilihanPercobaan317.java
@@ -14,7 +14,7 @@ public class PemilihanPercobaan317 {
             case '+':
             hasil = angka1 + angka2;
             System.out.println(angka1 + " + " + angka2 + "=" + hasil);
-            break;
+        
             case '-':
             hasil = angka1 + angka2;
             System.out.println(angka1 + " - " + angka2 + "=" + hasil);
@@ -26,7 +26,8 @@ public class PemilihanPercobaan317 {
             case '/':
             hasil = angka1 / angka2;
             System.out.println(angka1 + " / " + angka2 + "=" + hasil);
-            break; 
+            break;
+            
         }
         
     }
