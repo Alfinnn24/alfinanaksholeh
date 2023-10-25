dbf79596e2269ce514a4e89525841fd2e407760c
Alfinnn24 <144185072+Alfinnn24@users.noreply.github.com>
2023-10-25 10:03:47 +0700

    LinearSearch

--- LinearSearch16.java
+++ LinearSearch16.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class LinearSearch16 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jumlahElemen = input.nextInt();
+        int[] arrayInt = new int[jumlahElemen];
+        int key;
         int hasil = 0;
+
+        for (int i= 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen ke-"+i+" : ");
+            arrayInt[i] = input.nextInt();
+        }
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = input.nextInt();
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
@@ -10,6 +21,10 @@ public class LinearSearch16 {
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        if (hasil == 0) {
+            System.out.println("Key tidak ditemukan dalam array");
+        } else {
+            System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        }
     }
 }
